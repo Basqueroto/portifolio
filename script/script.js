bc4255030d0a29c979224a5d46255ecc8ea85085
basqueroto <basqueroto.felipe125@gmail.com>
2023-04-15 23:26:11 -0300

    inicio da animacao inicial

--- script/script.js
+++ script/script.js
@@ -109,19 +109,76 @@ particlesJS("particles-container", {
         "retina_detect": true
       });
   var count_particles, stats, update;
-  stats = new Stats();
-  stats.setMode(0);
-  stats.domElement.style.position = "absolute";
-  stats.domElement.style.left = "0px";
-  stats.domElement.style.top = "0px";
-  document.body.appendChild(stats.domElement);
   count_particles = document.querySelector(".js-count-particles");
   update = function () {
-    stats.begin();
-    stats.end();
     if (window.pJSDom[0].pJS.particles && window.pJSDom[0].pJS.particles.array) {
       count_particles.innerText = window.pJSDom[0].pJS.particles.array.length;
     }
     requestAnimationFrame(update);
   };
   requestAnimationFrame(update); 
+// ----------------------------------aparecer o texto---------------------------
+const typedTextSpan = document.querySelector(".typed-text");
+const cursorSpan = document.querySelector(".cursor");
+
+const textArray = ["hard", "fun", "a journey", "LIFE"];
+const typingDelay = 200;
+const erasingDelay = 100;
+const newTextDelay = 2000; // Delay between current and next text
+let textArrayIndex = 0;
+let charIndex = 0;
+
+function type() {
+  if (charIndex < textArray[textArrayIndex].length) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
+    charIndex++;
+    setTimeout(type, typingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    setTimeout(erase, newTextDelay);
+  }
+}
+
+function erase() {
+  if (charIndex > 0) {
+    if(!cursorSpan.classList.contains("typing")) cursorSpan.classList.add("typing");
+    typedTextSpan.textContent = textArray[textArrayIndex].substring(0, charIndex-1);
+    charIndex--;
+    setTimeout(erase, erasingDelay);
+  } 
+  else {
+    cursorSpan.classList.remove("typing");
+    textArrayIndex++;
+    if(textArrayIndex>=textArray.length) textArrayIndex=0;
+    setTimeout(type, typingDelay + 1100);
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function() { // On DOM Load initiate the effect
+  if(textArray.length) setTimeout(type, newTextDelay + 250);
+});
+
+// ----------------------abrir a foto ----------------
+
+window.addEventListener("scroll", () => {
+    console.log( window.innerHeight)
+    console.log( window.pageYOffset)
+       var first = document.getElementById("apresentacao-txt")
+       var divP = document.getElementById("apresentacao")
+       var fundo = document.getElementsByClassName("particles-js-canvas-el")
+       var nav = document.getElementById("vamos")
+       if (window.pageYOffset > 5){
+        console.log("img apareceu")
+        divP.className = "aberto"
+        nav.className = "aberto"
+        fundo.className = "dim"
+       }
+       else {
+        console.log("img voltou")
+        divP.className = "fechado"
+        nav.className = "fechado"
+        fundo.className = "aum"
+       }
+})
